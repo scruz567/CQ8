590d365598c7b4a0ff452add712503ceec205105
scruz567 <120342729+scruz567@users.noreply.github.com>
2023-12-06 12:33:59 -0500

    Add files via upload

---
+++ Staircase.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    // Write your code here
+        char[] stairs = new char[n];
+        for (int i =0; i< n; i++){
+            for (int j = 0; j < n; j++){
+                if ((j+i+1) < n)
+                    stairs[j] = ' ';
+                else{
+                    stairs[j] = '#';
+                }
+        }
+        System.out.println((stairs));
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
